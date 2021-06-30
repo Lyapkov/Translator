8c1d10067f1e5e1c8c119ec6cbd4a239dcdcaa83
Lyapk <Elena525658>
2021-06-30 23:09:36 +0300

    Lesson 2

---
+++ app/src/main/java/com/lyapkov/translator/di/ActivityModule.kt
@@ -0,0 +1,12 @@
+package com.lyapkov.translator.di
+
+import com.lyapkov.translator.view.main.MainActivity
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+@Module
+abstract class ActivityModule {
+
+    @ContributesAndroidInjector
+    abstract fun contributeMainActivity(): MainActivity
+}
---
+++ app/src/main/java/com/lyapkov/translator/viewmodel/BaseViewModel.kt
@@ -0,0 +1,21 @@
+package com.lyapkov.translator.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.lyapkov.translator.model.data.AppState
+import com.lyapkov.translator.rx.SchedulerProvider
+import io.reactivex.disposables.CompositeDisposable
+
+abstract class BaseViewModel<T : AppState>(
+    protected val liveDataForViewToObserve: MutableLiveData<T> = MutableLiveData(),
+    protected val compositeDisposable: CompositeDisposable = CompositeDisposable(),
+    protected val schedulerProvider: SchedulerProvider = SchedulerProvider()
+) : ViewModel() {
+
+    abstract fun getData(word: String, isOnline: Boolean)
+
+    override fun onCleared() {
+        compositeDisposable.clear()
+    }
+}
